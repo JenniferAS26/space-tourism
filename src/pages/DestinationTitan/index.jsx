4498e645f6c6925157e6190ba2a26efb14ad43a0
Mrinconrincon <rinconrinconmonica@gmail.com>
2023-10-03 17:48:30 -0500

    Create inicial Titan page

---
+++ src/pages/DestinationTitan/index.jsx
@@ -0,0 +1,35 @@
+import imagetitan from '../../assets/destination/image-titan.png'
+import style from './style.module.scss'
+
+
+const DestinationTitan = () => {
+  return (
+    <div>
+        <div>
+        <h1 className={style.title}>01 PICK YOUR DESTINATION</h1>
+        <img src={imagetitan} alt="" />
+        </div>
+        <div>
+          EARTH
+          MOON
+          EUROPA
+          TITAN
+        </div>
+        <div>
+        <h1>TITAN</h1>
+        </div>
+        <div>
+        <p> The only moon Known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, ypu get striking views of the Eings of Saturn.</p>
+        </div>
+        <div>
+          AVG. DISTANCE
+          1.6 BIL. KM
+          </div>
+          <div>
+          EST. TRAVEL TIME 7 YEARS
+        </div>
+    </div>
+  )
+}
+
+export default DestinationTitan
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from '../pages/Layout'
 import Home from '../pages/Home'
+import DestinationTitan from '../pages/DestinationTitan'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path='/' element={<Home />} />
+        <Route path='/destination-titan' element={<DestinationTitan />} />
         <Route path='destination' />
       </Route>
     </Routes>
