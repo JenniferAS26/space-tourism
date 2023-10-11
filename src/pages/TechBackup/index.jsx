c20ecca1bd72e473a40c9756877f559c2293c8c8
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-10-11 03:53:54 -0500

    update Technology component and Header component

--- src/pages/TechBackup/index.jsx
+++ src/pages/TechBackup/index.jsx
@@ -1,128 +1,54 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Header from '../../components/Header'
-import { technology } from '../../resources'
-import oneLandscape from '../../assets/technology/image-launch-vehicle-landscape.jpg'
-import onePortrait from '../../assets/technology/image-launch-vehicle-portrait.jpg'
-import twoLandscape from '../../assets/technology/image-spaceport-landscape.jpg'
-import twoPortrait from '../../assets/technology/image-spaceport-portrait.jpg'
-import threeLandscape from '../../assets/technology/image-space-capsule-landscape.jpg'
-import threePortrait from '../../assets/technology/image-space-capsule-portrait.jpg'
+import data from '../../data/data.json'
 import './styles.scss'
-import TechBackupLayout from '../../components/TechBackupLayout'
 
 const TechBackup = () => {
-  const [chooseTech, setChooseTech] = useState('one')
-  const changeImage = () => {
-    if (chooseTech === 'one') {
-      return (
-        <div>
-          <img className='block lg:hidden' src={oneLandscape} alt='spacecraft picture' />
-          <img className='hidden lg:block' src={onePortrait} alt='spacecraft picture' />
-        </div>
-      )
-    } else if (chooseTech === 'two') {
-      return (
-        <div>
-          <img className='block lg:hidden' src={twoLandscape} alt='spacecraft picture' />
-          <img className='hidden lg:block' src={twoPortrait} alt='spacecraft picture' />
-        </div>
-      )
-    } else if (chooseTech === 'three') {
-      return (
-        <div>
-          <img className='block lg:hidden' src={threeLandscape} alt='spacecraft picture' />
-          <img className='hidden lg:block' src={threePortrait} alt='spacecraft picture' />
-        </div>
-      )
-    }
-  }
-  const changeSpacecraftInfo = () => {
-    if (chooseTech === 'one') {
-      return (
-        <TechBackupLayout
-          name={technology.one.name}
-          description={technology.one.description}
-        />
-      )
-    } else if (chooseTech === 'two') {
-      return (
-        <TechBackupLayout
-          name={technology.two.name}
-          description={technology.two.description}
-        />
-      )
-    } else if (chooseTech === 'three') {
-      return (
-        <TechBackupLayout
-          name={technology.three.name}
-          description={technology.three.description}
-        />
-      )
-      
-    }
-  }
+  const technologies = data.technology
+  console.log(technologies)
+  const [value, setValue] = useState(0)
+  const { name, images, description } = technologies[value]
+
+  const [windowSize, setwindowSize] = useState(window.innerWidth)
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setwindowSize(window.innerWidth)
+    })
+  }, [])
 
   return (
-    <section className='technology w-full h-full'>
+    <section className='technology'>
       <Header />
-      <div className='technology__spacecraft-container'>
-        <h2 className='technology__spacecraft-container--title'><span>03</span> SPACE LAUNCH 101</h2>
-        <div className='technology__spacecraft-container--content'>
-          <div className='image'>
-            {changeImage()}
-          </div>
-          <div className='flex items-center'>
-            <div className='lg:flex flex-row'>
-              <div className='flex lg:flex-col justify-center lg:justify-between py-8 md:py-14 lg:py-3 lg:mr-8'>
-                <span className={
-                  `flex justify-center items-center 
-                  text-[16px] lg:text-[32px] 
-                  lg:leading-[37px] leading-[18px]
-                  tracking-[1px]
-                  ring-offset-blue-100
-                  w-[40px] lg:w-[80px] lg:h-[80px] h-[40px] 
-                  rounded-full
-                  cursor-pointer
-                  ${chooseTech === 'one' ? 'bg-white' : 'bg-gray-600 hover:bg-white'}
-                  `}
-                  onClick={() => setChooseTech('one')}
+      <div className='technology__content-container'>
+        <h2 className='technology__content-container--title'><span>03</span> SPACE LAUNCH 101</h2>
+        <div className='technology__content-container--dynamic-content'>
+          <img 
+            className='image' 
+            src={windowSize > 1440 ? images.portrait : images.landscape} 
+            alt={name} 
+          />
+          <div className='information'>
+            <div className='information__buttons-container'>
+              {
+                technologies.map((item, index) => (
+                  <button
+                    key={index}
+                    className='information__buttons-container--button'
+                    onClick={() => setValue(index)}
+                    style={{
+                      backgroundColor: index === value ? 'white' : 'transparent',
+                      color: index === value ? '#0B0D17' : 'white'
+                    }}
                   >
-                    1
-                  </span>
-                  <span className={
-                  `flex justify-center items-center 
-                  text-[16px] lg:text-[32px] 
-                  lg:leading-[37px] leading-[18px]
-                  tracking-[1px]
-                  ring-offset-blue-100
-                  w-[40px] lg:w-[80px] lg:h-[80px] h-[40px] 
-                  rounded-full
-                  cursor-pointer
-                  ${chooseTech === 'two' ? 'bg-white' : 'bg-gray-600 hover:bg-white'}
-                  `}
-                  onClick={() => setChooseTech('two')}
-                  >
-                    2
-                  </span>
-                  <span className={
-                  `flex justify-center items-center 
-                  text-[16px] lg:text-[32px] 
-                  lg:leading-[37px] leading-[18px]
-                  tracking-[1px]
-                  ring-offset-blue-100
-                  w-[40px] lg:w-[80px] lg:h-[80px] h-[40px] 
-                  rounded-full
-                  cursor-pointer
-                  ${chooseTech === 'three' ? 'bg-white' : 'bg-gray-600 hover:bg-white'}
-                  `}
-                  onClick={() => setChooseTech('three')}
-                  >
-                    3
-                  </span>
-              </div>
-              <div className='information w-full h-full'>
-                {changeSpacecraftInfo()}
-              </div>
+                    {index + 1}
+                  </button>
+                ))
+              }
+            </div>
+            <div className='information__content'>
+              <h4 className='information__content--title'>The terminology...</h4>
+              <h2 className='information__content--name'>{name}</h2>
+              <p className='information__content--description'>{description}</p>
             </div>
           </div>
         </div>
